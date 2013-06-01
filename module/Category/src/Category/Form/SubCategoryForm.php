6e08f439a980170f3a0ecd6d004a7881a4c697e9
Tom Flick <cubeflick@gmail.com>
2013-06-01 18:03:15 +0530

    Some more files are commited

---
+++ module/Category/src/Category/Form/SubCategoryForm.php
@@ -0,0 +1,62 @@
+<?php 
+namespace Category\Form; 
+
+use Zend\Captcha; 
+use Zend\Form\Element; 
+use Zend\Form\Form; 
+
+class SubCategoryForm extends Form 
+
+{ 
+    public function __construct($name = null) 
+    { 
+        parent::__construct('subcategory'); 
+        
+        $this->setAttribute('method', 'post'); 
+
+        $this->add(array(
+        		'name' => 'cname',
+        		'type' => 'Zend\Form\Element\Text',
+        		'attributes' => array(
+        				'class' => 'inp-form',
+        				'placeholder' => 'Name',
+        				'required' => 'required',
+        		),
+        		'options' => array(
+        				'label' => 'Name',
+        		),
+        ));
+
+        $this->add(array(
+        		'name' => 'parent_id',
+        		'type' => 'Zend\Form\Element\Select',
+        		'attributes' => array(
+        				'placeholder' => 'Name',
+        				'required' => 'required',
+        		),
+        		'options' => array(
+        				'label' => 'Parent Category',
+        		),
+        ));
+        
+        $this->add(array(
+        		'name' => 'description',
+        		'type' => 'Zend\Form\Element\Textarea',
+        		'attributes' => array(
+        				'class' => 'form-textarea',
+        				'required' => 'required',
+        		),
+        		'options' => array(
+        				'label' => 'Paragraph',
+        		),
+        )); 
+        
+        $this->add(array(
+            'name' => 'submit',
+            'type' => 'Zend\Form\Element\Submit',
+            'attributes' => array('value' => 'Add', 'class'=>'submit-login'),
+            'options' => array(),
+        ));
+         
+    } 
+} 
