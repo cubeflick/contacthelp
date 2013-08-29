a884bb93651e1cd09b8815f8b020a475623c854a
Tom Flick <cubeflick@gmail.com>
2013-08-29 13:30:45 +0530

    Type ahead is included

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,5 +1,23 @@
 $(function() {
 	$('#navigation ul li:last').addClass('last')
+	
+
+	 var availableTags = [ "ActionScript", "AppleScript", "Asp", "BASIC", "C",
+		"C++", "Clojure", "COBOL", "ColdFusion", "Erlang", "Fortran",
+		"Groovy", "Haskell", "Java", "JavaScript", "Lisp", "Perl", "PHP",
+		"Python", "Ruby", "Scala", "Scheme" ];
+	
+	$("#company").autocomplete({
+		source: "/application/index/searchlisting",
+		 select: function( event, ui ) {
+		 console.log( ui.item ?
+		 "Selected: " + ui.item.value + " aka " + ui.item.id :
+		 "Nothing selected, input was " + this.value );
+		 }
+	});
+	
+	
+	
 });
 
 $(window).load(function() {
